703ace2da265ede77516977df5698bf030f92aa7
Gustavo Adrien Polli <gapolli@yahoo.com.br>
2020-06-24 08:36:46 -0300

    Initial code and README.md structures created

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SIRmulator
+
 SIRmulator is a small command line epidemiological simulator written in C based on the SIR (Susceptible, Infected and Recovered) Model for analyzing the spread of an infectious disease.
+
+## How to run (Unix)
+
+- Compile the source with:
+
+  `$ gcc sir.c`
+
+- Execute the program and provide the necessary parameters
+
+  `$ ./a.out param_1 param_2 ... param_n`
+
+## Contributors (sorted alphabetically):
+
+- [Gustavo Adrien Polli](https://github.com/gapolli)
+- [Victor Gomes Sampaio](https://github.com/VictorGom3s)
+
+## Possible future implementations:
---
+++ sir.c
@@ -0,0 +1,57 @@
+/************************ DESCRIPTION SECTION *************************/
+
+/*
+ * Unicamp Faculty of Technology - FT
+ *
+ * SI010 - Data Structures II - 1st Half 2020
+ *
+ * Developers:
+ * 	Gustavo Adrien Polli	217357
+ * 	Victor Gomes Sampaio	225133
+ *
+ * Description:
+ * SIRmulator is a small command line epidemiological simulator
+ * written in C based on the SIR (Susceptible, Infected and Recovered)
+ * Model for analyzing the spread of an infectious disease.
+ */
+
+
+/************************* LIBRARIES SECTION **************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/************************* CONSTANTS SECTION **************************/
+
+// constants here
+
+
+/************************* STRUCTURES SECTION *************************/
+
+// structs here
+
+
+/************************* CODE CONVENTIONS ***************************/
+
+// code conventions here
+ 
+
+/************************** HEADERS SECTION ***************************/
+
+// function headers here
+
+
+/*********************** MAIN FUNCTION SECTION ************************/
+
+int main(int argc, char* argv[]){
+	
+	// code blocks here
+	
+	return 0; //need to return system status
+}
+
+
+/************************* FUNCTIONS SECTION **************************/
+
+// functions here
